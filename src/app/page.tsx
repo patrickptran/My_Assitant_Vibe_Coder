c6a27977f885f967c66db0e4fd3b188ff0ce9420
MangTran <83230443+MangTran@users.noreply.github.com>
2025-09-23 16:22:47 +0200

    try client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import prisma from "@/lib/db";
+"use client";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
-const Page = async () => {
-  const posts = await prisma.post.findMany();
+const Page = () => {
+  const trpc = useTRPC();
+  const { data, error, isLoading } = useQuery(
+    trpc.createAI.queryOptions({ text: "My AI from tRPC" })
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  // localhost:3000/trpc/createAI?body={text:"My AI from tRPC"}
   return (
     <div>
-      <h1>Users</h1>
-      <ul>{JSON.stringify(posts)}</ul>
+      <h1>{JSON.stringify(data)}</h1>
     </div>
   );
 };
