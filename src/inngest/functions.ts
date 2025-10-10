fadfd6e0d319061629641220ebcc9edccc2557d2
MangTran <83230443+MangTran@users.noreply.github.com>
2025-10-10 17:24:13 +0200

    add agent tools to manipulate the files created by agent inngest, TODO add test case

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,7 +1,8 @@
-import { createAgent, openai } from "@inngest/agent-kit";
+import { createAgent, createTool, openai } from "@inngest/agent-kit";
 import { inngest } from "./client";
 import { Sandbox } from "@e2b/code-interpreter";
 import { getSandbox } from "./utils";
+import { z } from "zod";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -17,7 +18,84 @@ export const helloWorld = inngest.createFunction(
       system:
         "You are an expert TypeScript programmer. You write readable, maintainable code. You write simple Next.js snippets. You only respond with code, no explanations.",
       model: openai({ model: "gpt-4o" }),
+      tools: [
+        createTool({
+          name: "terminal",
+          description: "A sandbox environment for running code snippets",
+          // @ts-expect-error the conflict between zod and inngest types
+          parameters: z.object({
+            command: z.string(),
+          }),
+
+          handler: async ({ command }, { step }) => {
+            return await step?.run("terminal", async () => {
+              const buffers = { stdout: "", stderr: "" };
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const result = await sandbox?.commands.run(command, {
+                  // maybe we want to capture stdin in the future?
+                  stdin: true,
+                  onStdout: (data: string) => {
+                    buffers.stdout += data;
+                  },
+                  onStderr: (data: string) => {
+                    buffers.stderr += data;
+                  },
+                });
+                return result?.stdout || "";
+              } catch (error) {
+                console.log(
+                  `Error running command: ${error} \n stdout: ${buffers.stdout} \n stderr: ${buffers.stderr}`
+                );
+                return `Error running command: ${error} \n stdout: ${buffers.stdout} \n stderr: ${buffers.stderr}`;
+              }
+            });
+          },
+        }),
+        createTool({
+          name: "createOrUpdateFiles",
+          description: " Create or update files in the sandbox",
+          // @ts-expect-error the conflict between zod and inngest types
+          parameters: z.object({
+            files: z.array(z.object({ path: z.string(), content: z.string() })),
+          }),
+          handler: async ({ files }, { step, network }) => {
+            /*
+            / app/page.tsx --> "<div>Hello World</div>"
+            / app/about/page.tsx --> "<div>About Us</div>"
+            */
+            const newFiles = await step?.run(
+              "create-or-update-files",
+              async () => {
+                try {
+                  const sandbox = await getSandbox(sandboxId);
+                  const updatedFiles: string[] = network.state.data.files || [];
+                  for (const file of files) {
+                    await sandbox?.files.write(file.path, file.content);
+                    updatedFiles[file.path] = file.content;
+                  }
+                  return updatedFiles;
+                } catch (e) {
+                  console.log("Error creating or updating files", e);
+                }
+              }
+            );
+            if (typeof newFiles === "object") {
+              network.state.data.files = newFiles;
+            }
+          },
+        }),
+        createTool({
+          name: "readFiles",
+          description: "Read files from the sandbox",
+          // @ts-expect-error the conflict between zod and inngest types
+          parameters: z.object({
+            paths: z.array(z.string()),
+          }),
+        }),
+      ],
     });
+
     const { output } = await codeAgent.run(
       `Write the following snippets: ${event.data.value}`
     );
